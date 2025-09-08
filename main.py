c05d8e428f24ee1beb4bd262f6a5e172123086b2
matheuspperes <matheuspalacio1@gmail.com>
2025-09-08 12:22:27 -0300

    feat: stanza similarity functions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from extraction.website import get_song_html
-from process.lyrics import organize_song_lyrics, parse_chords_and_lyrics, analyze_stanzas
+from process.lyrics import organize_song_lyrics, parse_chords_and_lyrics, analyze_stanzas, set_stanza_default
 from process.make_file import make_pdf
+from process.similarity import analyze_stanzas_similarity
 
 url = "https://www.cifraclub.com.br/fernandinho/nada-alem-do-sangue/"
 
@@ -11,8 +12,12 @@ if __name__ == "__main__":
     song_lines = parse_chords_and_lyrics(song_html)
 
     formatted_lyrics = organize_song_lyrics(song_lines)
-    sequence, id_map = analyze_stanzas(formatted_lyrics)
 
+    individual_results, groups = analyze_stanzas_similarity(formatted_lyrics)
+    if individual_results and groups:
+        set_stanza_default(groups, formatted_lyrics)
+
+    sequence, id_map = analyze_stanzas(formatted_lyrics)
     print(f"Ordem: {sequence}")
 
     for i, stanza in enumerate(formatted_lyrics):
