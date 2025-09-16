5fab4f78b92fa7fd0bed4d411a31fdd2822ef3ac
matheuspperes <matheuspalacio1@gmail.com>
2025-09-16 16:18:42 -0300

    feat: stanza break by user

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
 from extraction.website import get_song_html
+from logger import logger
 from process.lyrics import organize_song_lyrics, parse_chords_and_lyrics, analyze_stanzas, set_stanza_default
 from process.make_file import make_pdf
 from process.similarity import analyze_stanzas_similarity
-from process.stanza_scanning import analyze_repetition
+from process.stanza_scanning import analyze_repetition, break_stanzas
 
-url = "https://www.cifraclub.com.br/fernandinho/nada-alem-do-sangue/"
+url = input("Cole a URL aqui: ")
 
 song_name = f"{url.split('/')[-2]}"
 
 if __name__ == "__main__":
+    logger.info(f"Começando processo na URL: {url}")
     song_html = get_song_html(url)
+
+    logger.info(f"Fazendo separação de linhas")
     song_lines = parse_chords_and_lyrics(song_html)
 
+    logger.info(f"Organizando cifra em estrofes")
     formatted_lyrics = organize_song_lyrics(song_lines)
 
+    print("===================")
+    break_check = input("Esse passo por ser útil depois da visualização do pdf...\n"
+                        "Deseja quebrar alguma estrofe em partes?(vazio=NAO): ")
+    while break_check:
+        break_stanzas(formatted_lyrics)
+        break_check = input("Continuar quebrando estrofes?(vazio=NAO): ")
+
+    logger.info(f"Encontrando repetições")
     filtered_formatted_lyrics = analyze_repetition(formatted_lyrics)
 
+    logger.info(f"Analisando similaridade entre estrofes")
     individual_results, groups = analyze_stanzas_similarity(filtered_formatted_lyrics)
     if individual_results and groups:
         set_stanza_default(groups, filtered_formatted_lyrics)
 
+    logger.info("Analisando estrofes para gerar sequência e PDF")
     sequence, id_map = analyze_stanzas(filtered_formatted_lyrics)
     print(f"Ordem: {sequence}")
 
-    for i, stanza in enumerate(filtered_formatted_lyrics):
-        print(f"Stanza {i + 1}: {stanza}")
-
+    logger.info("Gerando PDF")
     make_pdf(song_name, sequence, id_map)
     # raw_pdf(song_lines, f"{song_name}.pdf")
+    logger.info("Processo concluído")
--- process/stanza_scanning.py
+++ process/stanza_scanning.py
@@ -1,4 +1,7 @@
-def analyze_repetition(lyrics:list[list[str]]) -> list:
+from logger import logger
+
+
+def analyze_repetition(lyrics: list[list[str]]) -> list:
     """Analyzes if there is a repetition that can be filtered
 
     :param lyrics: song lyrics
@@ -7,7 +10,7 @@ def analyze_repetition(lyrics:list[list[str]]) -> list:
     new_lyrics = []
 
     for stanza in lyrics:
-        if stanza[0][0] in ("[", "("):
+        if stanza[0][0] == "(":
             new_lyrics.append(stanza)
             continue
 
@@ -20,7 +23,8 @@ def analyze_repetition(lyrics:list[list[str]]) -> list:
 
     return new_lyrics
 
-def identify_repetition_inside_stanza(stanza:list[str]) -> list | tuple:
+
+def identify_repetition_inside_stanza(stanza: list[str]) -> list | tuple:
     """Identifies a repetition by analyzing blocks of lines
 
     :param stanza: lines of a stanza
@@ -48,10 +52,40 @@ def identify_repetition_inside_stanza(stanza:list[str]) -> list | tuple:
                 if remaining_length % pattern_length == 0:
                     num_repetitions = lenght_stanza // pattern_length
                     separated_stanzas = [result[qty_repetition * pattern_length:(qty_repetition + 1) * pattern_length]
-                                 for qty_repetition in range(num_repetitions)]
+                                         for qty_repetition in range(num_repetitions)]
 
                     return tuple(separated_stanzas)
 
         count += 1
 
     return result
+
+
+def break_stanzas(formatted_lyrics: list) -> None:
+    """Breaks stanzas in a specific line
+
+    :param formatted_lyrics:
+    """
+    logger.info(f"Existem {len(formatted_lyrics)} estrofes")
+    print("Aqui estao o começo delas:")
+    for index, stanza in enumerate(formatted_lyrics):
+        length = 4
+        len_stanza = len(stanza)
+
+        if len_stanza < length:
+            length = len(stanza)
+
+        print(f"Estrofe {index} - {stanza[:length]}")
+
+    print("===================")
+    stanza_index = int(input("Qual estrofe deseja alterar?: "))
+    for index, line in enumerate(formatted_lyrics[stanza_index]):
+        print(f"Linha {index}: {line}")
+
+    line_index = int(input("Em qual linha deseja começar uma nova estrofe?: "))
+
+    second_stanza = formatted_lyrics[stanza_index][line_index:]
+
+    formatted_lyrics[stanza_index] = formatted_lyrics[stanza_index][:line_index]
+    formatted_lyrics.insert(stanza_index + 1, second_stanza)
+    return
